3a818d9774f065df31fede7262d7bb721906be92
rajoriaakash <akashrajoria250103@gmail.com>
2022-06-19 03:55:12 +0530

    Fix null operator error

--- formente/lib/Screens/Home/show_entries.dart
+++ formente/lib/Screens/Home/show_entries.dart
@@ -26,61 +26,65 @@ class _ShowEntriesState extends State<ShowEntries> {
   }
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: entries,
+    return FutureBuilder<List<DiaryEntryModel>>(
+        future: entries as Future<List<DiaryEntryModel>>,
         builder: (BuildContext context,AsyncSnapshot<List<DiaryEntryModel>> entries){
-          return Scaffold(
-            appBar: AppBar(
-              elevation: 0,
-              backgroundColor: Color(0xff2AB5E1),
-              leading: IconButton(
-                icon: Icon(
-                    Icons.arrow_back
-                ),
-                onPressed: (){
-                  Navigator.pop(context);
-                },
-              )
-            ),
-            body: Container(
-            height: MediaQuery.of(context).size.height,
-            decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    colors:[Color(0xff2AB5E1),Color(0xff19C5FC).withOpacity(0.2)],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter
-                )
-            ),
-              padding: EdgeInsets.symmetric(vertical: 40, horizontal: 20),
-              child: Column(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(bottom: 30),
-                    child: Text(
-                      "Journal Entries",
-                      style: TextStyle(
-                        fontSize: 30,
-                        color: Colors.white70
-                      ),
+          if(entries.hasData) {
+            return Scaffold(
+              appBar: AppBar(
+                  elevation: 0,
+                  backgroundColor: Color(0xff2AB5E1),
+                  leading: IconButton(
+                    icon: Icon(
+                        Icons.arrow_back
                     ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  )
+              ),
+              body: Container(
+                  height: MediaQuery
+                      .of(context)
+                      .size
+                      .height,
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: [
+                            Color(0xff2AB5E1),
+                            Color(0xff19C5FC).withOpacity(0.2)
+                          ],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter
+                      )
                   ),
+                  padding: EdgeInsets.symmetric(vertical: 40, horizontal: 20),
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(bottom: 30),
+                        child: Text(
+                          "Recent Entries",
+                          style: TextStyle(
+                              fontSize: 30,
+                              color: Colors.white70
+                          ),
+                        ),
+                      ),
 
-                  ListView.builder(
-                    shrinkWrap: true,
-                      itemCount: entries.data?.length,
-                      itemBuilder: (BuildContext context,int index){
-                        return GestureDetector(
-                          // onTap:(){
-                          //   Navigator.push(context, MaterialPageRoute(builder: (context)=>FilteredPosts(genre: posts.data![index].genreNeeded, genreNeeded:posts.data![index].genre )));
-                          // },
-                            child: DiaryEntryTile(entry: entries.data![index])
-                        );
-                      }
-                  ),
-                ],
-              )
-            ),
-          );
+                      ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: entries.data?.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return DiaryEntryTile(entry: entries.data![index]);
+                          }
+                      ),
+                    ],
+                  )
+              ),
+            );
+          }else
+            return Container();
         }
     );
   }
