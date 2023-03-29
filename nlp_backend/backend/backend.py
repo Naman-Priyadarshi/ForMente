a39b9449c71821af0855ab621251e1a44758b904
Saransh Chopra <saransh0701@gmail.com>
2023-03-29 15:02:57 +0530

    Update backend.py

--- nlp_backend/backend/backend.py
+++ nlp_backend/backend/backend.py
@@ -5,9 +5,19 @@ import numpy as np
 from pydantic import BaseModel
 from fastapi import FastAPI, File, UploadFile
 from sklearn.feature_extraction.text import CountVectorizer
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+# Annoying CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 labels = {
     0: "anger",
